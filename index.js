7fe5916cc66e2ad95cf2acfef679bdba7ba966ea
Junior_Kamanda <jkamanda062gmail.com>
2024-01-20 23:49:03 +0100

    Hellium Javascript

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
-
+const cursors = document.querySelectorAll(".cursor")
 // Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+window.addEventListener("mousemove",(e)=>{
+    cursors.forEach((cursor)=>{
+        cursor.style.top = e.y +"px";
+        cursor.style.left = e.x +"px";
+    })
+    
+})
 
 // S'assurer que les liens sont clickables
 
